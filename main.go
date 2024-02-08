0206cb062f26852d9072da3220da7ac56d0e2f8a
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-02-08 15:34:58 -0500

    cosa

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"CrudApi/models"
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	var book = models.Book{
+		Title:  "libro",
+		Author: "stuz zu",
+	}
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.IndentedJSON(http.StatusOK, book)
+
+	})
+	r.GET("/generate_uuid", func(c *gin.Context) {
+		// Generate a new UUID
+		newUUID := uuid.New()
+
+		c.JSON(200, gin.H{
+			"uuid": newUUID.String(),
+		})
+	})
+	r.Run()
 }
