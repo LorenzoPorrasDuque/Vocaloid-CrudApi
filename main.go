737e2b611ae952997a5e32c788649d3aa21b977d
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-02-10 11:10:34 -0500

    Db connection and errors
    I dont know why, but this shit isnt connecting at all

--- main.go
+++ main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"CrudApi/models"
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 	"net/http"
 )
 
 func main() {
-	var book = models.Book{
+	var _ = models.Book{
 		Title:  "libro",
 		Author: "stuz zu",
 	}
+	dsn := "host=localhost user=postgres dbname=test sslmode=disable password=mysecretpassword"
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+
+	fmt.Println("Connected to the database")
+	db.AutoMigrate(&models.Book{})
 
 	r := gin.Default()
 
@@ -18,5 +29,7 @@ func main() {
 		c.IndentedJSON(http.StatusOK, book)
 
 	})
+
 	r.Run()
+
 }
