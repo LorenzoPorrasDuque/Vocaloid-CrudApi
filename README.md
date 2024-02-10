cba4a0e0f12174bff77f8ee999c9e1891183e122
Lorenzo Porras Duque <lorenzo.porras.duque@gmail.com>
2024-02-09 22:27:03 -0500

    reformat and nothing else

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CrudApi
+This is a simple CRUD API using Gin and docker with a Postgres database.
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"CrudApi/models"
 	"github.com/gin-gonic/gin"
-	"github.com/google/uuid"
 	"net/http"
 )
 
@@ -12,18 +11,12 @@ func main() {
 		Title:  "libro",
 		Author: "stuz zu",
 	}
+
 	r := gin.Default()
+
 	r.GET("/", func(c *gin.Context) {
 		c.IndentedJSON(http.StatusOK, book)
 
-	})
-	r.GET("/generate_uuid", func(c *gin.Context) {
-		// Generate a new UUID
-		newUUID := uuid.New()
-
-		c.JSON(200, gin.H{
-			"uuid": newUUID.String(),
-		})
 	})
 	r.Run()
 }
